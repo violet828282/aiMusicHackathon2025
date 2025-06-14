aa4c1792e35082f65238c6e0253d0b391bfeb205
violet828282 <dev.mihyeon@gmail.com>
2025-06-14 22:21:05 +0900

    add feature/add-handLandMaker

--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -4,19 +4,26 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(req: NextRequest) {
   //This is AI endpoint.
-  try {
-    const requestBody = await req.json()
-    const externalResponse = await fetch('url', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(requestBody),
-    })
 
+
+  try {
+    const formData = await req.formData();
+    const imageFile = formData.get('image') as File | null;
+
+    const externalFormData = new FormData();
+    externalFormData.append('image', imageFile);
+
+    const externalResponse = await fetch('https://tien-ms-7e06.tail1bbb7.ts.net/predict', {
+      method: 'POST',
+      body: externalFormData,
+    });
     const data = await externalResponse.json()
 
-    return NextResponse.json(data)
+    return NextResponse.json(data as {
+      boxes: Array<any>,
+      labels: Array<string>,
+      masks: Array<any>
+    })
 
   } catch (err) {
     console.error('[API ERROR]:', err)
